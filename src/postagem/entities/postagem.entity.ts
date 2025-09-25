3b72b90df5c7703335dfac19de092455e7448700
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-09-25 18:38:58 -0300

    src: Adicionando restante do crud de postagem, criando classe tema

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,9 +2,11 @@ import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Tema } from '../../tema/entities/tema.entity';
 
 @Entity({ name: 'tb_postagens' }) // Define o nome da tabela no banco de dados
 export class Postagem {
@@ -21,4 +23,9 @@ export class Postagem {
 
   @UpdateDateColumn()
   data: Date;
+
+  @ManyToOne(() => Tema, (tema) => tema.postagem, {
+    onDelete: 'CASCADE',
+  })
+  tema: Tema;
 }
